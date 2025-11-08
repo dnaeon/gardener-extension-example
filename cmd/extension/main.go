eee2b7a0a8a4ebb71e279cf816f5a9dc4161fbeb
Marin Atanasov Nikolov <dnaeon@gmail.com>
2025-11-08 11:25:33 +0200

    cmd/extension: initial commit of main package

---
+++ cmd/extension/main.go
@@ -0,0 +1,32 @@
+// TODO: command to generate the controller-registration manifest
+
+package main
+
+import (
+	"os"
+
+	"github.com/urfave/cli/v3"
+	ctrl "sigs.k8s.io/controller-runtime"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
+
+	managercmd "gardener-extension-example/cmd/extension/internal/manager"
+	"gardener-extension-example/internal/pkg/version"
+)
+
+func main() {
+	app := &cli.Command{
+		Name:                  "gardener-extension-example",
+		Version:               version.Version,
+		EnableShellCompletion: true,
+		Usage:                 "an example gardener extension",
+		Commands: []*cli.Command{
+			managercmd.New(),
+		},
+	}
+
+	ctx := ctrl.SetupSignalHandler()
+	if err := app.Run(ctx, os.Args); err != nil {
+		ctrllog.Log.Error(err, "failed to start extension")
+		os.Exit(1)
+	}
+}
