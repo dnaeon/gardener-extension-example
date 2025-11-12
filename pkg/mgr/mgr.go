997f3adfb321483648ccbfd0c0290b3b419cf7d8
Marin Atanasov Nikolov <dnaeon@gmail.com>
2025-11-12 09:26:39 +0200

    Add license headers

--- pkg/mgr/mgr.go
+++ pkg/mgr/mgr.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // Package mgr provides utility functions for creating [manager.Manager] using a
 // functional options API.
 package mgr
