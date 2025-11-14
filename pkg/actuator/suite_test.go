4747d20590b0b0a28121f78ab33d86881a6ea2c4
Marin Atanasov Nikolov <dnaeon@gmail.com>
2025-11-14 11:18:06 +0200

    actuator/test: configure gardener client for testing purposes

--- pkg/actuator/suite_test.go
+++ pkg/actuator/suite_test.go
@@ -6,6 +6,7 @@ package actuator_test
 
 import (
 	"context"
+	"path/filepath"
 	"testing"
 
 	"github.com/go-logr/logr"
@@ -13,6 +14,8 @@ import (
 	. "github.com/onsi/gomega"
 
 	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
+	corev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	gardenerclientset "github.com/gardener/gardener/pkg/client/core/clientset/versioned"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -22,12 +25,13 @@ import (
 )
 
 var (
-	ctx       context.Context
-	cancel    context.CancelFunc
-	testEnv   *envtest.Environment
-	cfg       *rest.Config
-	k8sClient client.Client
-	logger    logr.Logger
+	ctx            context.Context
+	cancel         context.CancelFunc
+	testEnv        *envtest.Environment
+	cfg            *rest.Config
+	k8sClient      client.Client
+	logger         logr.Logger
+	gardenerClient gardenerclientset.Interface
 )
 
 func TestActuators(t *testing.T) {
@@ -41,14 +45,16 @@ var _ = BeforeSuite(func() {
 
 	ctx, cancel = context.WithCancel(context.TODO())
 
+	Expect(corev1beta1.AddToScheme(scheme.Scheme)).To(Succeed())
 	Expect(extensionscontroller.AddToScheme(scheme.Scheme)).To(Succeed())
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
 		Scheme: scheme.Scheme,
 		CRDDirectoryPaths: []string{
-			"../../test/manifests/crd/gardener.cloud/core/v1beta1",
-			"../../test/manifests/crd/gardener.cloud/extensions/v1alpha1",
+			filepath.Join("..", "..", "test", "manifests", "crd", "gardener.cloud", "core", "v1"),
+			filepath.Join("..", "..", "test", "manifests", "crd", "gardener.cloud", "core", "v1beta1"),
+			filepath.Join("..", "..", "test", "manifests", "crd", "gardener.cloud", "extensions", "v1alpha1"),
 		},
 		ErrorIfCRDPathMissing: true,
 	}
@@ -61,6 +67,10 @@ var _ = BeforeSuite(func() {
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
+
+	gardenerClient, err = gardenerclientset.NewForConfig(cfg)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(gardenerClient).NotTo(BeNil())
 })
 
 var _ = AfterSuite(func() {
